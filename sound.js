a5d202cfcf91c2a99c9bf7ddad7f963a5e33fda6
Claudia <claudia.cedfeldt@gmail.com>
2017-01-22 16:32:00 -0800

    playing with visuals

--- sound.js
+++ sound.js
@@ -1,8 +1,31 @@
+'use strict';
+
+var fft, mySound, analyzer;
+
 function preload() {
   mySound = loadSound('https://mp3d.jamendo.com/download/track/1314413/mp32/');
 }
 
 function setup() {
+  // Set up createCanvas
+  createCanvas(500, 500);
+  background(0);
+
+  // Create new fft to analyze mp3
+  fft = new p5.FFT();
+
+  // create a new Amplitude analyzer
+  analyzer = new p5.Amplitude();
+
+  // Patch the input to an volume analyzer
+  analyzer.setInput(mySound);
+
+  // Play tunes
   mySound.setVolume(0.5);
   mySound.play();
+}
+
+function draw() {
+  var rms = analyzer.getLevel();
+  ellipse(width/2, height/2, rms*500, rms*500);
 }
